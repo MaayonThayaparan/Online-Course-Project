a27cd9f808deeb4ac1d8991a198716a2651b99f3
Maayon Thayaparan <maayon.thayaparan@gmail.com>
2023-07-20 19:00:51 -0400

    Adding admin for Choices and Questions

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
+    extra = 5
+
+class QuestionInline(admin.QuestionInline):
+    model = Question
+    extra = 5
+
+class ChoiceInline(admin.ChoiceInline):
+    model = Choice
     extra = 5
 
 
@@ -19,7 +27,16 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
     list_display = ['title']
+
+class QuestionAdmin(admin.ModelAdmin):
+    inLines = [ChoiceInline]
+    list_display = ('question_text')
+    
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text','is_correct')
 
 
 # <HINT> Register Question and Choice models here
@@ -28,3 +45,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
